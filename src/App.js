241b8b2a3fedafc1ff8761143ced353e766dae34
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-08-11 20:53:24 +0400

    add routes to shop and cart functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 /* eslint-disable react/react-in-jsx-scope */
 import "./App.css";
+import "normalize.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import HomePage from "./home/home";
 import ShopPage from "./shop/shop";
+import booksToSell from "./shop/booksToSell/books";
+import ProductDetails from "./shop/components/productDetails";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (id, amount) => {
+    const productToAdd = {};
+    productToAdd.id = Number(id);
+    productToAdd.amount = Number(amount);
+    const newCart = [...cart];
+    const productAlreadyAdded = cart.find((product) => {
+      return product.id === productToAdd.id;
+    });
+    console.log(productAlreadyAdded);
+    if (productAlreadyAdded != null) {
+      newCart.productAlreadyAdded.amount += productToAdd.amount;
+    } else {
+      newCart.push(productToAdd);
+    }
+
+    setCart([newCart]);
+  };
+  const deleteFromCart = (id) => {
+    setCart(
+      cart.filter((product) => {
+        return product.id !== Number(id);
+      }),
+    );
+  };
+
+  useEffect(() => {
+    console.log(cart);
+  });
+
   return (
     <BrowserRouter>
       <Link to="/">
@@ -16,7 +51,13 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/shop" element={<ShopPage books={booksToSell} />} />
+        <Route
+          path="/shop/view/:productid"
+          element={
+            <ProductDetails products={booksToSell} addToCart={addToCart} />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
